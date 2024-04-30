e83f3b022009f1080ff59662d3790cbe11ac27a7
Your Name <you@example.com>
2024-04-29 20:27:16 -0600

    Correccion

--- src/Componentes/Contact.jsx
+++ src/Componentes/Contact.jsx
@@ -1,5 +1,5 @@
 import github from "../assets/Social/github.svg"
-import linkedln from "../assets/Social/linkedin.svg"
+import LinkedIn from "../assets/Social/linkedin.svg"
 import mail from "../assets/Social/mail.svg"
 import { cn } from "../utils/cn";
 
@@ -16,14 +16,14 @@ function Contact() {
                 </a>
                 <a href="https://www.linkedin.com/in/josué-guido-beteta" className={cn("inline-flex items-center py-2 px-2 border-gray-600 rounded-md hover:bg-gray-200 gap-2 md:gap-0 relative z-20")}>
                     <div className="flex flex-row justify-center items-center text-center">
-                        <img src={linkedln} className="w-20 h-10 rounded-md hover:bg-gray-200 " />
-                        <p className=" hidden md:block">Linkedln</p>
+                        <img src={LinkedIn} className="w-20 h-10 rounded-md hover:bg-gray-200 " />
+                        <p className=" hidden md:block">LinkedIn</p>
                     </div>
                 </a>
                 <a href="mailto:josuguido@gmail.com" className={cn("inline-flex items-center py-2 px-2 border-gray-600 rounded-md hover:bg-gray-200 gap-2 md:gap-0 relative z-20")}>
                     <div className="flex flex-row justify-center items-center text-center">
                         <img src={mail} className="w-20 h-10" />
-                        <p className=" hidden md:block">Contáctame</p>
+                        <p className=" hidden md:block">Contácteme</p>
                     </div>
                 </a>
             </section>
--- src/Componentes/Layout.jsx
+++ src/Componentes/Layout.jsx
@@ -4,18 +4,15 @@ import Header from "./Header";
 import Principal from "./Principal";
 import Proyectos from "./Proyectos"
 import Slider from "./Slider";
-import ThemeColor from "./ThemeColor";
 
 function Layout() {
     return (
         <main>
-            <ThemeColor>
                 <Header />
                 <Principal />
                 <About />
                 <Proyectos />
                 <Slider />
-            </ThemeColor>
             <Footer />
         </main>
     );
